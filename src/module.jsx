87f029196739bb0a12e809e489070727ba049ec6
nightingale16066 <nightingale16066@gmail.com>
2022-11-18 22:43:38 +0300

    react-template

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import parrot from './img/parrot.jpg';
+
+const Module = () => (
+  <>
+    <h1>Hello, React!</h1>
+    <img width={500} src={parrot}/>
+  </>
+);
+
+export default Module;
